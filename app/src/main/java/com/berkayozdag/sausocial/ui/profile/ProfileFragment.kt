d3bb5d71c43310a2dd815fcd106115696381b49e
Berkayozdag81 <berkayozdag2001@gmail.com>
2023-03-25 00:47:07 +0300

    [feat] add the profile screen.

--- app/src/main/java/com/berkayozdag/sausocial/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/berkayozdag/sausocial/ui/profile/ProfileFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.berkayozdag.sausocial.R
+import com.berkayozdag.sausocial.common.HomeMockData
+import com.berkayozdag.sausocial.common.Post
 import com.berkayozdag.sausocial.databinding.FragmentProfileBinding
+import com.berkayozdag.sausocial.ui.home.adapters.PostsAdapter
 
 class ProfileFragment : Fragment() {
 
@@ -16,23 +23,48 @@ class ProfileFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val adapter = PostsAdapter()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        setupRecyclerview()
+        onItemClick()
+
         val root: View = binding.root
 
-        val textView: TextView = binding.textViewProfile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadPosts(HomeMockData.getPost())
+    }
+
+    private fun setupRecyclerview() = with(binding) {
+        val layoutManager = LinearLayoutManager(requireContext())
+        val itemDecoration: RecyclerView.ItemDecoration = DividerItemDecoration(
+            context,
+            DividerItemDecoration.VERTICAL
+        )
+        rwProfileUserPost.addItemDecoration(itemDecoration)
+        rwProfileUserPost.layoutManager = layoutManager
+    }
+
+    private fun loadPosts(posts: List<Post>) = with(binding) {
+        adapter.setData(posts)
+        rwProfileUserPost.adapter = adapter
+    }
+
+    private fun onItemClick() {
+        adapter.onItemClicked = {
+            findNavController().navigate(R.id.action_navigation_home_to_postDetailFragment)
+        }
     }
 
     override fun onDestroyView() {

3320b76056f6800a26cdf7bd6ae23af1f09ce0ce
ozturkselim99 <ozturkselim99@gmail.com>
2023-06-05 15:50:56 +0300

    [feat] get groups.

--- app/src/main/java/com/berkayozdag/sausocial/ui/groups/GroupsFragment.kt
+++ app/src/main/java/com/berkayozdag/sausocial/ui/groups/GroupsFragment.kt
@@ -5,19 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.berkayozdag.sausocial.common.Group
-import com.berkayozdag.sausocial.common.HomeMockData
+import com.berkayozdag.sausocial.data.NetworkResponse
 import com.berkayozdag.sausocial.databinding.FragmentGroupsBinding
+import com.berkayozdag.sausocial.model.profile.ProfileResponse
 import com.berkayozdag.sausocial.ui.groups.adapters.GroupsAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GroupsFragment : Fragment() {
 
     private var _binding: FragmentGroupsBinding? = null
     private val binding get() = _binding!!
     private val adapter = GroupsAdapter()
+    private val groupsViewModel: GroupsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,14 +29,15 @@ class GroupsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGroupsBinding.inflate(inflater, container, false)
-        setupRecyclerview()
-        onItemClick()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loadGroups(HomeMockData.getGroups())
+        groupsViewModel.getUsers()
+        setupRecyclerview()
+        onItemClick()
+        setupObserves()
     }
 
     private fun setupRecyclerview() = with(binding) {
@@ -45,7 +50,23 @@ class GroupsFragment : Fragment() {
         recyclerViewGroups.layoutManager = layoutManager
     }
 
-    private fun loadGroups(groups: List<Group>) = with(binding) {
+    private fun setupObserves() {
+        groupsViewModel.usersResponse.observe(viewLifecycleOwner) {
+            when (it) {
+                is NetworkResponse.Loading -> {
+
+                }
+                is NetworkResponse.Success -> {
+                    loadGroups(it.data.filter { it.isGroup })
+                }
+                is NetworkResponse.Error -> {
+
+                }
+            }
+        }
+    }
+
+    private fun loadGroups(groups: List<ProfileResponse>) = with(binding) {
         adapter.items = groups
         recyclerViewGroups.adapter = adapter
     }
---
+++ app/src/main/java/com/berkayozdag/sausocial/ui/groups/GroupsViewModel.kt
@@ -0,0 +1,26 @@
+package com.berkayozdag.sausocial.ui.groups
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.berkayozdag.sausocial.data.NetworkResponse
+import com.berkayozdag.sausocial.data.repository.SocialAppRepository
+import com.berkayozdag.sausocial.model.profile.ProfileResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GroupsViewModel @Inject constructor(
+    private val repository: SocialAppRepository
+) : ViewModel() {
+
+    private val _usersResponse = MutableLiveData<NetworkResponse<List<ProfileResponse>>>()
+    val usersResponse: LiveData<NetworkResponse<List<ProfileResponse>>> = _usersResponse
+
+    fun getUsers() = viewModelScope.launch {
+        _usersResponse.value = NetworkResponse.Loading
+        _usersResponse.value = repository.getUsers()
+    }
+}
